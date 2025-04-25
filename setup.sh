3b6fda4c84c1f57ebd2c75906b192ad50bdb44ac
Tom Palmer <remlapmot@hotmail.com>
2025-04-25 10:21:07 +0100

    Add install nbstata line

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 uv venv --python 3.13
 source .venv/bin/activate
 uv pip install nbstata jupyterlab matplotlib numpy jupyterlab_stata_highlight2
+python -m nbstata.install
 
 quarto render
 deactivate

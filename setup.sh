92f912511540aaaf2bffa96bcbacb80c216d9030
Tom Palmer <remlapmot@hotmail.com>
2025-04-25 10:40:41 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+# Important, this must be set globally, i.e. in .zprofile - so that pystata Python library can be found
+# export PYTHONPATH=/Applications/Stata/utilities
+
 uv venv --python 3.13
 source .venv/bin/activate
 uv pip install nbstata jupyterlab matplotlib numpy jupyterlab_stata_highlight2

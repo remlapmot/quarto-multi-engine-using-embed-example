2888c72c97e0e03b714fd86e19cd4a642b16d110
Tom Palmer <remlapmot@hotmail.com>
2025-04-29 22:01:04 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
-# Important, this must be set globally, i.e. in .zprofile - so that pystata Python library can be found
+# Important, this must be set globally, i.e. in .zprofile - so that Python can find the pystata package
 export PYTHONPATH=/Applications/Stata/utilities
-
-# And within Stata - that must be picking up a Python installation with a libpython.dylib
-# i.e. `python query` must return something valid
 
 uv venv --python 3.13
 source .venv/bin/activate
